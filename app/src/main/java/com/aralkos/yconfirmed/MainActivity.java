b0275f605dc551b382cd06a38b083f88b4443cf9
“whoisYeshua” <alien1998@yandex.ru>
2020-06-10 12:05:28 +0300

    add captcha activity & captcha spinner

--- app/src/main/java/com/aralkos/yconfirmed/MainActivity.java
+++ app/src/main/java/com/aralkos/yconfirmed/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void toSettings(View view) {
@@ -21,4 +26,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         Animatoo.animateFade(this);
     }
+
 }
---
+++ app/src/main/java/com/aralkos/yconfirmed/captcha.java
@@ -0,0 +1,14 @@
+package com.aralkos.yconfirmed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class captcha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_captcha);
+    }
+}
